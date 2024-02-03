e41986f788dc4949037c3c6646dec18ea1d3ad70
Nikita56807 <152766431+Nikita56807@users.noreply.github.com>
2024-02-03 16:04:21 +0900

    Add files via upload

--- Timer.cs
+++ Timer.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 public class Timer : MonoBehaviour
 {
     public int minutes;
-    public float seconds; 
+    public float seconds;
     public string sceneName;
+    public int roundSeconds;
+    public TMP_Text timer_;
     // Start is called before the first frame update
     void Start()
     {
-        minutes = 1;
-        seconds = 60;
+        timer_ = GameObject.Find("Textimer").GetComponent<TMP_Text>();
+        minutes = 0;
+        seconds = 10;
     }
 
     // Update is called once per frame
@@ -27,8 +32,18 @@ public class Timer : MonoBehaviour
         {
             EditorSceneManager.LoadScene(sceneName);
             print("scene reload");
+            minutes = 1;
+            seconds = 60;
         }
- 
+         roundSeconds = Mathf.RoundToInt(seconds);
+        timer_.text = minutes.ToString() + " : " + roundSeconds.ToString();
     }
 }
+
+       
+
+    
+   
+
+
 
